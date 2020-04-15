264362960cf8c7e2095bf39baea82172e2297492
Myungsun Baek <baekboys@naver.com>
2020-04-16 01:38:30 +0900

    구글 로그인 처리

--- src/main/java/com/baek/springboot/web/IndexController.java
+++ src/main/java/com/baek/springboot/web/IndexController.java
@@ -1,10 +1,11 @@
 package com.baek.springboot.web;
 
+import com.baek.springboot.config.auth.dto.SessionUser;
+import com.baek.springboot.domain.user.User;
 import com.baek.springboot.service.posts.PostService;
 import com.baek.springboot.web.dto.PostsResponseDto;
 import com.baek.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
+
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
